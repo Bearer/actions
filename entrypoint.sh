36f490ccd1dbf2d9f75b8dd783dd63c27671ada8
Cédric Fabianski <cfabianski@me.com>
2022-04-08 12:23:59 +0200

    chore: use input to checkout the required version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
-cd /app/bearer-broker_linux_amd64/
+echo "Using version ${VERSION}"
+
+curl https://bearer-cli-binaries.s3.eu-west-1.amazonaws.com/${VERSION}/bearer-broker_linux_amd64.zip --output bearer.zip
+
+unzip bearer.zip
+
+chmod u+x -R bearer-broker_linux_amd64/
+
+cd bearer-broker_linux_amd64/
 ./broker
